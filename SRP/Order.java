e925a6b1e07c99b91cf0de71a2f8d9a6d73e35ad
zheivgt <133962917+zheivgt@users.noreply.github.com>
2023-10-18 08:47:30 +0700

    Class Order Perubahan1

--- SRP/Order.java
+++ SRP/Order.java
@@ -1,25 +1,17 @@
 class Order { 
  void calculateTotalSum() { 
-/*...*/ 
  } 
+ 
  void getItems() { 
-/*...*/ 
  } 
+ 
  void getItemCount() { 
-/*...*/ 
  } 
+ 
  void addItem(Item item) { 
-/*...*/ 
+ 
  } 
+ 
  void deleteItem(Item item) { 
-/*...*/ 
  } 
- void printOrder() { 
-/*...*/ 
- } 
- void showOrder() { 
-/*...*/ 
- } 
- void getDailyHistory() {} 
- void getMonthlyHistory() {} 
-} 
+}
