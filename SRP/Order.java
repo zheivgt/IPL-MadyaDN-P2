3fa1ac359f2865762c492857a9deac410d7c4db8
zheivgt <133962917+zheivgt@users.noreply.github.com>
2023-10-18 08:46:30 +0700

    Class Order Kesatu

---
+++ SRP/Order.java
@@ -0,0 +1,25 @@
+class Order { 
+ void calculateTotalSum() { 
+/*...*/ 
+ } 
+ void getItems() { 
+/*...*/ 
+ } 
+ void getItemCount() { 
+/*...*/ 
+ } 
+ void addItem(Item item) { 
+/*...*/ 
+ } 
+ void deleteItem(Item item) { 
+/*...*/ 
+ } 
+ void printOrder() { 
+/*...*/ 
+ } 
+ void showOrder() { 
+/*...*/ 
+ } 
+ void getDailyHistory() {} 
+ void getMonthlyHistory() {} 
+} 
